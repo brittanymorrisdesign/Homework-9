4afd8bc396d38b734e82a7dec377f630826c9eda
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-01-29 18:02:43 -0500

    Retrieved data from api, generated html doc

--- Develop/index.js
+++ Develop/index.js
@@ -8,9 +8,12 @@ const gs = require('github-scraper');
 // Convert to pdf packages, then open
 const convertFactory = require('electron-html-to');
 const open = require('open');
-const pdfTemplate = require('./generateHTML.js');
+const generateHTML = require('./generateHTML.js');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // Prompt user for Github username and favorite color
+
 inquirer
   .prompt([
     {
@@ -21,21 +24,44 @@ inquirer
     {
       type: 'list',
       message: 'favColor',
-      name: 'stack',
+      name: 'color',
       choices: ['Red', 'Blue', 'Green', 'Orange'],
     },
   ])
-  .then(function(res) {
+  .then(function(userResponse) {
     // Urls for axios requests
-    const githubUrl = `https://api.github.com/users/${res.username}`;
-    const githubRepos = `https://api.github.com/users/${res.username}/repos`;
-    // Axios to retrieve data from Github api
-    axios.get(githubUrl).then(function(res) {
-      console.log(res.data);
+    const queryUrl = `https://api.github.com/users/${userResponse.username}/repos?per_page=100`;
+    const queryUrl2 = `https://api.github.com/users/${userResponse.username}/repos`;
+    const starredUrl = `https://api.github.com/users/${userResponse.username}/starred?per_page=100`;
 
-      // Convert html to pdf
-      const conversion = convertFactory({
-        converterPath: convertFactory.converters.PDF,
-      });
+    // Axios to retrieve data from Github api
+    axios.get(queryUrl).then(function(userName) {
+      const userInfo = userName.data;
+      console.log(userInfo);
+      axios
+        .get(queryUrl2)
+        .then(function(result) {
+          console.log(result);
+          const makeHTML = generateHTML(userResponse);
+          return writeFileAsync(`${userInfo.name}.html`, makeHTML);
+        }) // Call generateHTML to make into HTML doc
+        .then(function(genHTML) {
+          console.log('Successfully wrote to index.html');
+          const conversion = convertFactory({
+            converterPath: convertFactory.converters.PDF,
+          });
+
+          conversion({ html: genHTML }, function(err, result) {
+            if (err) {
+              return console.error(err);
+            }
+
+            result.stream.pipe(fs.createWriteStream(`"test.pdf"`));
+            conversion.kill();
+          });
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
     });
   });
