f59bba025c6005420514a2b7b6103fe46d81ef95
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-01-28 21:52:35 -0500

    Installed npm packages

---
+++ Develop/index.js
@@ -0,0 +1,18 @@
+// npm packages
+const axios = require("axios");
+const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
+const open = require("open");
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
