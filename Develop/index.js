15dce95f6595c704d977b54daf022b5595896beb
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-01-29 10:26:47 -0500

    Updated prompts to console log

--- Develop/index.js
+++ Develop/index.js
@@ -1,38 +1,31 @@
 // npm packages
-const axios = require("axios");
-const inquirer = require("inquirer");
-const util = require("util");
-const fs = require("fs");
-const open = require("open");
+const axios = require('axios');
+const inquirer = require('inquirer');
+const util = require('util');
+const fs = require('fs');
+const open = require('open');
+const pdfTemplate = require('./generateHTML.js');
 
-
-function userInfo() {
-inquirer.prompt([
+inquirer
+  .prompt([
     {
-      type: "input",
-      name: "username",
-      message: "What is your github username?"
+      type: 'input',
+      name: 'username',
+      message: 'What is your github username?',
     },
     {
-      type: "list",
-      message: "favColor",
-      name: "stack",
-      choices: [
-        "Red", 
-        "Blue", 
-        "Green", 
-        "Orange"
-      ]
+      type: 'list',
+      message: 'favColor',
+      name: 'stack',
+      choices: ['Red', 'Blue', 'Green', 'Orange'],
     },
-
-      ]);
-    }
-
-
-function writeToFile(fileName, data) {
- 
-}
-
-function init() {
-userInfo()
-init();
+  ])
+  .then(function(res) {
+    // Urls for axios requests
+    const githubUrl = `https://api.github.com/users/${res.username}`;
+    const githubRepos = `https://api.github.com/users/${res.username}/repos`;
+    // Axios to retrieve data from Github api
+    axios.get(githubUrl).then(function(res) {
+      console.log(res.data);
+    });
+  });
